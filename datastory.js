a006f9f370d9c3a56b6836c380c8fe67de18383f
Brecht <brechtvermeire@gmail.com>
2019-01-17 15:22:56 +0000

    viz updates

--- datastory.js
+++ datastory.js
@@ -1,19 +1,17 @@
-// on document ready
 $(function() {
-    // extract the spreadsheet ID from the URL (if any, otherwise use the demo)
-    var spreadsheet_id = (getParameterByName('spreadsheet_id') ? getParameterByName('spreadsheet_id') : '151wYbceiQIj8NEBiLCZNDFX1CEtirddu7tCOU4NR6AA')
-        // update the menu
+    var spreadsheet_id = (getParameterByName('id') ? getParameterByName('id') : '151wYbceiQIj8NEBiLCZNDFX1CEtirddu7tCOU4NR6AA')
+    var style_mode = (getParameterByName('style') ? getParameterByName('style') : 'light').toLowerCase()
+    var primary_color = (getParameterByName('color') ? getParameterByName('color') : 'white').toLowerCase()
     $("#sheet_id").val(spreadsheet_id)
-    $("#primary_color").val("#7e64e0")
+    $("#primary_color").val(primary_color) 
 
-    // create the URL to fetch from
-    var spreadsheet_public_url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheet_id + '/od6/public/values?alt=json'
-        // load the datastory
-    loadDatastory(spreadsheet_public_url)
+    if (history.pushState) {
+        var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?id=' + spreadsheet_id;
+        window.history.pushState({path:newurl},'',newurl);
+    }
 
-    // listener to update sheet ID
+    loadDatastory(spreadsheet_id, style_mode)
     $("#sheet_id").keypress(function(a) {
-        // if enter
         if (a.keyCode == 13) {
             $("#sheet_id_label").html("Updating....")
             spreadsheet_public_url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheet_id + '/od6/public/values?alt=json'
@@ -25,7 +23,6 @@ $(function() {
         }
     })
 
-    // listener to primary color
     $("#primary_color").keypress(function(a) {
         // if enter
         if (a.keyCode == 13) {
@@ -45,32 +42,52 @@ $(function() {
     })
 
     // style change
-    $('#dark_style').change(
-        function() {
-            if ($(this).is(':checked')) {
-                $(".story_container").css("color", "#FFFFFF")
-                $(".mdl-card").css("background-color", "#959595")
-                $(".mdl-tabs__tab-bar").css("background-color", "#BBB")
-                $(".story_container").css("background-color", "#353535")
-            }
-            $("#dark_style").attr("disabled", "disabled")
-        });
+    $('#dark_style').change(function() {
+        if (this.checked == true) {
+            setDarkMode()
+        } else {
+            setLightMode()
+        }
+    });
 
     // remove download bar
     $('#remove_download').change(
         function() {
             if ($(this).is(':checked')) {
-                $(".mdl-tabs__tab-bar").remove()
+                $(".mdl-tabs__tab-bar").hide()
+            } else {
+                $(".mdl-tabs__tab-bar").show()
             }
-            $("#remove_download").attr("disabled", "disabled")
         });
 })
 
 
 
-function loadDatastory(url) {
-    var card_template = `<section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp story_card"> <div class="mdl-card mdl-cell mdl-cell--12-col"> <div class="mdl-card__supporting-text"> <h4 class="card_title">{{card_title}}</h4> <p>{{card_description}}</p></div><div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect"> <div class="mdl-tabs__panel is-active" id="card_{{card_number}}_viz"> <div class="mdl-card__supporting-text"> <iframe src='{{instance_url}}/embed/public/{{look_public_id}}' height='400px' frameborder='0'></iframe> </div></div><div class="mdl-tabs__tab-bar"> <a href="#card_{{card_number}}_viz" class="mdl-tabs__tab is-active">Visualization</a> <a href="{{instance_url}}/looks/{{look_public_id}}.csv?apply_formatting=true&apply_vis=true&download=true" target="_blank" download class="mdl-tabs__tab">Data</a> </div></div></section>`
-    $.getJSON(url, function(data) {
+function loadDatastory(id) {
+    var spreadsheet_public_url = 'https://spreadsheets.google.com/feeds/list/' + id + '/od6/public/values?alt=json'
+    var card_template = `
+        <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp story_card">
+        <div class="mdl-card mdl-cell mdl-cell--12-col">
+            <div class="mdl-card__supporting-text card_description">
+                <h4 class="card_title">{{card_title}}</h4>
+                {{ card_description }}
+            </div>
+        </div>
+        <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+            <div class="mdl-tabs__panel is-active" id="card_{{card_number}}_viz">
+                <div class="mdl-card__supporting-text story_viz"> <iframe src='{{instance_url}}/embed/public/{{look_public_id}}' height='400px' frameborder='0'></iframe>
+                </div>
+            </div>
+            <div class="mdl-tabs__tab-bar">
+                <a href="#card_{{card_number}}_viz" class="mdl-tabs__tab is-active">Visualization</a>
+                <a href="{{instance_url}}/looks/{{look_public_id}}.csv?apply_formatting=true&apply_vis=true&download=true" download class="mdl-tabs__tab">Data</a>
+
+            </div>
+        </div>
+    </section>`
+
+
+    $.getJSON(spreadsheet_public_url, function(data) {
         var results = data.feed.entry
         var instance_url
         $.each(results, function(key, result) {
@@ -81,7 +98,7 @@ function loadDatastory(url) {
                     "story_description": result.gsx$description.$t
                 }
                 document.title = title_data.story_title
-                var title_template = `<h1 class="story_title" style="text-align: center;">{{ story_title }}</h1><p>{{ story_description }}</p>`
+                var title_template = `<h1 class="story_title" style="text-align: center;">{{ story_title }}</h1><p class="story_description">{{ story_description }}</p>`
                 var title = Mustache.render(title_template, title_data)
                 $(".story_title_container").append(title)
             } else {
@@ -96,13 +113,63 @@ function loadDatastory(url) {
                 $(".story_container").append(card)
             }
         })
-    })
+        if (history.pushState) {
+            var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?id=' + id;
+            window.history.pushState({path:newurl},'',newurl);
+        }
+    }).fail(function(d) {
+                var card_data = {
+                    "card_number": 1,
+                    "card_title": "Uh oh!",
+                    "card_description": "Couldn't find the spreadsheet, are you sure it's published?",
+                    "look_public_id": "",
+                    "instance_url": ""
+                }
+                var card = Mustache.render(card_template, card_data)
+                $(".story_container").append(card)
+            })
 }
 
 // extract spreadsheet ID from URL
 function getParameterByName(name) {
     var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search)
     return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
+}
+
+function updateUrlParameter(param, value) {
+    const regExp = new RegExp(param + "(.+?)(&|$)", "g");
+    const newUrl = window.location.href.replace(regExp, param + "=" + value + "$2");
+    window.history.pushState("", "", newUrl);
+}
+
+function showLinks() {
+    $(".mdl-tabs__tab-bar").show()
+}
+
+function hideLinks() {
+    $(".mdl-tabs__tab-bar").hide()
+}
+
+function setDarkMode() {
+        $(".story_title").css("color", "#FFF")
+        $(".card_title").css("color", "#FFF")
+        $(".story_container").css("color", "#FFFFFF")
+        $(".mdl-card").css("background-color", "#282828")
+        $(".mdl-card").css("border-color", "#282828")
+        $(".mdl-tabs__tab-bar").css("background-color", "#282828")
+        $(".mdl-tabs__tab").css("color", "#efefef")
+        $(".story_container").css("background-color", "#282828")
+}
+
+function setLightMode() {
+    $(".story_title").css("color", "#282828")
+    $(".card_title").css("color", "#282828")
+    $(".story_container").css("color", "#282828")
+    $(".mdl-card").css("background-color", "#FFF")
+    $(".mdl-card").css("border-color", "#FFF")
+    $(".mdl-tabs__tab-bar").css("background-color", "#FFF")
+    $(".mdl-tabs__tab").css("color", "#282828")
+    $(".story_container").css("background-color", "#FFF")
 }
 
 // clear the whole document
